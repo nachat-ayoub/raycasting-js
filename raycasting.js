877b9280fd57b7b2991229a3930f267321d1e37d
nachat-ayoub <ayoub.nachat.27@gmail.com>
2025-07-28 13:05:18 +0100

    finish casting rays in Horiz and Vert

--- raycasting.js
+++ raycasting.js
@@ -4,6 +4,23 @@ function toRad(deg) {
 	return ((deg * Math.PI) / 180);
 }
 
+function normalizeAngle(angle) {
+	angle = angle % (Math.PI * 2);
+	if (angle < 0)
+		angle += (Math.PI * 2);
+	return (angle);
+}
+
+function getDist(pt1, pt2) {
+	// get distance between 2 points:
+	return (
+		Math.sqrt(
+			Math.pow(pt2.y - pt1.y, 2) +
+			Math.pow(pt2.x - pt1.x, 2)
+		)
+	)
+}
+
 const UP_ARROW = 38;
 const DOWN_ARROW = 40;
 const LEFT_ARROW = 37;
@@ -26,9 +43,9 @@ class MyMap {
 	constructor() {
 		this.map = [
 			[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-			[1, 0, 0, 0, 'N', 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
-			[1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1],
-			[1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1],
+			[1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+			[1, 0, 1, 0, 0, 'N', 0, 0, 0, 1, 0, 1, 1, 0, 1],
+			[1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1],
 			[1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1],
 			[1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1],
 			[1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1],
@@ -54,18 +71,33 @@ class MyMap {
 			}
 		}
 	}
+
+	hasWallAt(x, y) {
+		let mapX = Math.floor(x / TILE_SIZE);
+		let mapY = Math.floor(y / TILE_SIZE);
+		if (this.map[mapY][mapX] == 1)
+			return true;
+		return false;
+	}
+
+	inBoundaries(x, y) {
+		return ((x >= 0 && x <= WINDOW_W) && (y >= 0 && y <= WINDOW_H));
+	}
 }
+
+const map = new MyMap();
+
 
 class Player {
 	constructor(tileX, tileY, map) {
 		this.size = TILE_SIZE * 0.2;
-		this.x = (tileX * TILE_SIZE) + (this.size * 0.7);
-		this.y = (tileY * TILE_SIZE) + (this.size * 0.7);
-		this.angle = toRad(45);
+		this.x = (tileX * TILE_SIZE) + (TILE_SIZE * 0.5);
+		this.y = (tileY * TILE_SIZE) + (TILE_SIZE * 0.5);
+		this.angle = toRad(190);
 		this.move = 0;
 		this.rotate = 0;
 		this.moveSpeed = 1.8;
-		this.rotateSpeed = 0.1;
+		this.rotateSpeed = 0.05;
 		this.map = map;
 	}
 	render() {
@@ -73,47 +105,20 @@ class Player {
 		fill("red");
 		noStroke();
 		circle(this.x, this.y, this.size);
+
+		stroke("red");
+		let x2 = player.x + Math.cos(this.angle) * 50;
+		let y2 = player.y + Math.sin(this.angle) * 50;
+		line(player.x, player.y, x2, y2);
 	}
 	update() {
 		let newX = this.x + Math.cos(this.angle) * this.move * this.moveSpeed;
 		let newY = this.y + Math.sin(this.angle) * this.move * this.moveSpeed;
-		if (!this.hasWallAt(newX, newY)) {
+		if (!map.hasWallAt(newX, newY)) {
 			this.x = newX;
 			this.y = newY;
 		}
 		this.angle += this.rotate * this.rotateSpeed;
-	}
-	hasWallAt(x, y) {
-		let radius = this.size / 2;
-
-		// Check multiple points around the player's circumference
-		let checkPoints = [
-			{ x: x, y: y }, // center
-			{ x: x + radius, y: y }, // right
-			{ x: x - radius, y: y }, // left
-			{ x: x, y: y + radius }, // bottom
-			{ x: x, y: y - radius }  // top
-		];
-
-		for (let point of checkPoints) {
-			let tileX = Math.floor(point.x / TILE_SIZE);
-			let tileY = Math.floor(point.y / TILE_SIZE);
-
-			if (tileX < 0 || tileX >= COLS || tileY < 0 || tileY >= ROWS) {
-				return true;
-			}
-
-			if (map.map[tileY][tileX] === 1) {
-				return true;
-			}
-		}
-		// x -= this.size/2;
-		// y -= this.size/2;
-		// let mapX = Math.floor(x/TILE_SIZE);
-		// let mapY = Math.floor(y/TILE_SIZE);
-		// if (this.map[mapY][mapX] == 1)
-		//     return true;
-		// return false;
 	}
 }
 
@@ -128,40 +133,152 @@ function getPlayerPos(map) {
 	}
 }
 
-const map = new MyMap(); // ✅ Don't forget this
 const player = new Player(...getPlayerPos(map.map), map.map); // ✅ Don't forget this
 let rays = [];
 
 class Ray {
-	constructor(player, rayAngle) {
-		this.pl = player;
-		this.rayAngle = rayAngle;
+	constructor(rayAngle) {
+		this.rayAngle = normalizeAngle(rayAngle);
+		this.distance = 0;
+		this.isFacingDown = this.rayAngle > 0 && this.rayAngle < Math.PI;
+		this.isFacingUP = !this.isFacingDown;
+		this.wallHitX = 0;
+		this.wallHitY = 0;
+		this.isFacingRight = this.rayAngle < Math.PI * 0.5 || this.rayAngle > Math.PI * 1.5;
+		this.isFacingLeft = !this.isFacingRight;
 	}
-	//   cast() {
-	//   }
+	cast() {
+		//======== check Horizontal interceptions: =======
+		let horiz = this.getHorizontalWallHit();
+
+		//======== check Vertical interceptions: =======
+		let vert = this.getVerticalWallHit();
+
+		if (vert.dist < horiz.dist) {
+			this.wallHitX = vert.x;
+			this.wallHitY = vert.y;
+		} else {
+			this.wallHitX = horiz.x;
+			this.wallHitY = horiz.y;
+		}
+
+	}
+
+	getHorizontalWallHit() {
+		let foundHit = false;
+		let hitX = 0;
+		let hitY = 0;
+		let stepX = 0;
+		let stepY = 0;
+		let intercX = 0;
+		let intercY = 0;
+
+		intercY = Math.floor(player.y / TILE_SIZE) * TILE_SIZE;
+		if (this.isFacingDown)
+			intercY += TILE_SIZE;
+		intercX = player.x + (intercY - player.y) / Math.tan(this.rayAngle);
+
+		stepY = TILE_SIZE;
+		stepY *= this.isFacingUP ? -1 : 1;
+
+		stepX = Math.abs(TILE_SIZE / Math.tan(this.rayAngle));
+		stepX *= this.isFacingLeft ? -1 : 1;
+
+		let nextIntercX = intercX;
+		let nextIntercY = intercY;
+
+		if (this.isFacingUP)
+			nextIntercY--;
+
+		while (map.inBoundaries(nextIntercX, nextIntercY)) {
+			if (map.hasWallAt(nextIntercX, nextIntercY)) {
+				foundHit = true;
+				hitX = nextIntercX;
+				hitY = nextIntercY;
+				break;
+			}
+			nextIntercX += stepX;
+			nextIntercY += stepY;
+		}
+		
+		return ({
+			foundHit,
+			x: hitX, y: hitY,
+			dist: foundHit ?
+				getDist({ x: player.x, y: player.y },
+					{ x: hitX, y: hitY })
+				: Number.MAX_VALUE
+		});
+	}
+
+	getVerticalWallHit() {
+		let foundHit = false;
+		let hitX = 0;
+		let hitY = 0;
+		let stepX = 0;
+		let stepY = 0;
+		let intercX = 0;
+		let intercY = 0;
+
+		intercX = Math.floor(player.x / TILE_SIZE) * TILE_SIZE;
+		if (this.isFacingRight)
+			intercX += TILE_SIZE;
+		intercY = player.y + (intercX - player.x) * Math.tan(this.rayAngle);
+
+		stepX = TILE_SIZE;
+		stepX *= this.isFacingLeft ? -1 : 1;
+
+		stepY = Math.abs(TILE_SIZE * Math.tan(this.rayAngle));
+		stepY *= this.isFacingUP ? -1 : 1;
+
+		let nextIntercX = intercX;
+		let nextIntercY = intercY;
+
+		if (this.isFacingLeft)
+			nextIntercX--;
+
+		while (map.inBoundaries(nextIntercX, nextIntercY)) {
+			if (map.hasWallAt(nextIntercX, nextIntercY)) {
+				foundHit = true;
+				hitX = nextIntercX;
+				hitY = nextIntercY;
+				break;
+			}
+			nextIntercX += stepX;
+			nextIntercY += stepY;
+		}
+
+		return ({
+			foundHit,
+			x: hitX, y: hitY,
+			dist: foundHit ?
+				getDist({ x: player.x, y: player.y },
+					{ x: hitX, y: hitY })
+				: Number.MAX_VALUE
+		});
+	}
+
 	render() {
-		stroke("green");
-		let x2 = this.pl.x + Math.cos(this.rayAngle) * 100;
-		let y2 = this.pl.y + Math.sin(this.rayAngle) * 100;
-		line(this.pl.x, this.pl.y, x2, y2);
+		stroke("orange");
+		line(player.x, player.y, this.wallHitX, this.wallHitY);
 	}
 }
 
 function castAllrays() {
-	let rayAngle = player.angle - (FOV / 2);
+	let startRayAngle = player.angle - (FOV / 2);
 	for (let i = 0; i < NUM_RAYS; i++) {
-		const ray = new Ray(player, rayAngle);
+		const ray = new Ray(startRayAngle);
+		ray.cast();
 		ray.render();
 		rays.push(ray);
-		rayAngle += RAY_ANGLE;
+		startRayAngle += RAY_ANGLE;
 	}
 	rays = [];
 }
 
 function keyPressed() {
-	log("Key Pressed: ", keyCode);
+	// log("Key Pressed: ", keyCode);
 	if (keyCode === UP_ARROW) {
-		log("Arrow up pressed");
 		player.move = 1;
 	} else if (keyCode === DOWN_ARROW) {
 		player.move = -1;
@@ -173,7 +290,7 @@ function keyPressed() {
 }
 
 function keyReleased() {
-	log("Key Released: ", keyCode);
+	// log("Key Released: ", keyCode);
 	if (keyCode === UP_ARROW || keyCode === DOWN_ARROW) {
 		player.move = 0;
 	} else if (keyCode === LEFT_ARROW || keyCode === RIGHT_ARROW) {
@@ -188,11 +305,13 @@ function setup() {
 function update() {
 	player.update();
 }
-
+let test = 0;
 function draw() {
 	update();
 	background(220);
-	map.render(); // ✅ Render the map
-	castAllrays();
+	map.render();
+	if (test == 0)
+		castAllrays();
+	// test = 1;
 	player.render();
 }
